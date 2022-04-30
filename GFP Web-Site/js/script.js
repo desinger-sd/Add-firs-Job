41291333fd520b437e5d3cc6bed11f7b8525f364
desinger-sd <104691269+desinger-sd@users.noreply.github.com>
2022-04-30 22:24:44 +0500

    Hello my subcribed This is my first job

---
+++ GFP Web-Site/js/script.js
@@ -0,0 +1,7 @@
+let preload = document.querySelector(".preload")
+let contPreloader = document.querySelector(".cont_preloader")
+
+window.onload = () => {
+    preload.classList.add("active_preload")
+    contPreloader.classList.add("hu")
+}
